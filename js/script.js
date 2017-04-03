227d22f1134143ad25107f2b462e05b6baf6ed3a
Nina Kin <nina@ninakin.com>
2017-04-03 23:07:07 +0000

    mapped the DTLA to Long Beach route

--- js/script.js
+++ js/script.js
@@ -8,22 +8,37 @@ L.mapbox.accessToken = 'pk.eyJ1IjoibWF0aWtpbjkiLCJhIjoiYjMyMjBjZTE4NWUxMDkxOWZjZ
 var mymap = L.map('map-display').setView([34.04, -118.25], 13);
 L.mapbox.styleLayer('mapbox://styles/matikin9/cim5bt1q100iy9jkpl7ff9d1h').addTo(mymap);
 
-var gpx = 'data/Morning_Ride.gpx'; // URL to your GPX file or the GPX itself
-new L.GPX(gpx, 
+var gpx1 = 'data/Morning_Ride.gpx'; // URL to your GPX file or the GPX itself
+var gpx2 = 'data/Dtla_Long_Beach.gpx'
+
+var greenBicycle = L.ExtraMarkers.icon({
+    icon: 'fa-bicycle',
+    markerColor: 'green',
+    shape: 'square',
+    prefix: 'fa'
+});
+
+var redBicycle = L.ExtraMarkers.icon({
+    icon: 'fa-bicycle',
+    markerColor: 'red',
+    shape: 'square',
+    prefix: 'fa'
+});
+
+new L.GPX(gpx1, 
     {
         async: true,
         marker_options: {
-            startIcon: L.ExtraMarkers.icon({
-                icon: 'fa-bicycle',
-                markerColor: 'green',
-                shape: 'square',
-                prefix: 'fa'
-            }),
-            endIcon: L.ExtraMarkers.icon({
-                icon: 'fa-bicycle',
-                markerColor: 'red',
-                shape: 'square',
-                prefix: 'fa'
-            })
+            startIcon: greenBicycle,
+            endIcon: redBicycle
+        }
+    }).addTo(mymap);
+    
+new L.GPX(gpx2, 
+    {
+        async: true,
+        marker_options: {
+            startIcon: greenBicycle,
+            endIcon: redBicycle
         }
     }).addTo(mymap);
